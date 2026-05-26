1fe27ea699722ac781226c7d74459ba9823c9082
username54321-dotcom <marwankam3l@gmail.com>
2026-05-26 22:27:47 +0300

    Refreshed Github Access Token

--- clients/github.ts
+++ clients/github.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import { Octokit } from "@octokit/rest";
 
 const githubToken =
-  process.env.GITHUB_TOKEN ||
-  "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";
+  "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";
 
 export const githubClient = new Octokit({ auth: githubToken });
