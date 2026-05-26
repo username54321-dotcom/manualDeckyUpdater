41d2447ee16c321529daf848f5e4f0ff66286461
username54321-dotcom <marwankam3l@gmail.com>
2026-05-26 22:41:22 +0300

    minor token patch

--- clients/github.ts
+++ clients/github.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 import { Octokit } from "@octokit/rest";
 
 const githubToken =
-  "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";
+  "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";
 
 export const githubClient = new Octokit({ auth: githubToken });
